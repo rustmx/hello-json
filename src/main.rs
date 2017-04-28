a52f03797889ef3f832a241439e4198f5a2e8600
Mario Martinez <hello@mariomartinez.space>
2017-04-27 23:18:26 -0500

    Comentarios agregados al código fuente.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+// rand nos va a ayudar a generar números aleatorios.
 extern crate rand;
+// iron es un web framework flexible orientado a middleware.
 extern crate iron;
+// rustc_serialize nos va a ayudar a serializar una struct a JSON.              
 extern crate rustc_serialize;
 
 use iron::prelude::*;
@@ -8,16 +11,29 @@ use iron::mime::Mime;
 use rand::Rng;
 use rustc_serialize::json;
 
+/*
+Declaración de un constante con el servidor y puerto de nuestro servicio.
+*/
 const MY_URL: &'static str = "localhost:3009";
 
+/*
+La siguiente estructura, JsonResponse, debe poder ser codificable o "encodable".
+Es por ello que le antecedemos un derive con RustcEncodable.
+*/
 #[derive(RustcDecodable, RustcEncodable)]
 struct JsonResponse{
     response: String
 }
 
+
+/*
+La función pick_response() crea un respuesta aleatoria de tipo String.
+*/
 fn pick_response() -> String {
+    // Creación de un número aleatorio.
     let num = rand::thread_rng().gen_range(1,6);
 
+    // Elección de un respuesta a partir del número aleatorio.
     let response = match num {
         1 => "Hola JSON",
         2 => "Estoy usando el crate Iron.",
